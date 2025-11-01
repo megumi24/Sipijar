faa22062d31db1b75b200b6395354c4fe120e018
Farhan Yahya Hidayat <farhan.yh@gmail.com>
2025-11-01 20:11:54 +0700

    feat(layout): add floating button to go to telegram bot chat

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -3,19 +3,31 @@ import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
 import { AppSidebarHeader } from '@/components/app-sidebar-header';
 import { type BreadcrumbItem } from '@/types';
+import { MessageSquare } from 'lucide-react';
+import { Button } from 'primereact/button';
 import { type PropsWithChildren } from 'react';
 
 export default function AppSidebarLayout({
-    children,
-    breadcrumbs = [],
+  children,
+  breadcrumbs = [],
 }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
-    return (
-        <AppShell variant="sidebar">
-            <AppSidebar />
-            <AppContent variant="sidebar" className="overflow-x-hidden">
-                <AppSidebarHeader breadcrumbs={breadcrumbs} />
-                {children}
-            </AppContent>
-        </AppShell>
-    );
+  return (
+    <AppShell variant="sidebar">
+      <AppSidebar />
+      <AppContent variant="sidebar" className="relative overflow-x-hidden">
+        <Button
+          tooltip="Tanya AI"
+          tooltipOptions={{ position: 'left' }}
+          aria-label="Chat"
+          rounded
+          raised
+          icon={() => <MessageSquare />}
+          className="fixed! right-4 bottom-4 z-10"
+          onClick={() => window.open('https://t.me/sipijar01_bot', '_blank')}
+        />
+        <AppSidebarHeader breadcrumbs={breadcrumbs} />
+        {children}
+      </AppContent>
+    </AppShell>
+  );
 }
--- resources/js/pages/sna/index.tsx
+++ resources/js/pages/sna/index.tsx
@@ -17,6 +17,12 @@ const SnaIndex = () => {
       <Head title="Network Mapping" />
       <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
         <NetworkGraph />
+        <iframe
+          title="Dashboard Metabase"
+          src="http://metabase.sipijar.my.id/public/dashboard/64e123da-62e4-49c2-adc7-7bcbd6aa2b49"
+          className="h-[85vh] w-full border-0"
+          allowTransparency
+        ></iframe>
       </div>
     </AppLayout>
   );
